068ce86d86af8de29e1f91ee6ad2dd693aad0c4f
Mugilan <mugilanmass23@gmail.com>
2019-07-21 20:47:54 +0530

    Create Grid of any size

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('Hello from console.log()');
+const container_div = document.getElementById('container');
+
+function createGrid(size, side) {
+  const grid = document.createElement('div');
+  grid.classList.add('grid');
+  container_div.appendChild(grid);
+  grid.style.setProperty('--sideSize', side);
+
+  for (let i = 0; i < size; i++) {
+    const block = document.createElement('div');
+    block.classList.add('block');
+    grid.appendChild(block);
+  }
+}
+
+let sideSize = 16;
+let size = sideSize ** 2;
+
+createGrid(size, sideSize);
